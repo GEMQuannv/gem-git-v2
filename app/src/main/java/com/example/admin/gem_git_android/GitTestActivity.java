f1bacffeb1e13dbc175827fc62e46b2c56c71a67
quannv@gemvietnam.com <quannv123>
2017-08-13 15:48:51 +0700

    change GitTestActivity

--- app/src/main/java/com/example/admin/gem_git_android/GitTestActivity.java
+++ app/src/main/java/com/example/admin/gem_git_android/GitTestActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 public class GitTestActivity extends AppCompatActivity {
@@ -24,6 +25,8 @@ public class GitTestActivity extends AppCompatActivity {
             .setAction("Action", null).show();
       }
     });
+
+    Log.e("error", "log  test");
   }
 
 }
